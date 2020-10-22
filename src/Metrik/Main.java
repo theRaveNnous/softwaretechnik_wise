f73435349d9d4d7b3110c024c8e8e6601308f118
u33998@hs-harz.de <u33998@hs-harz.de>
2020-10-22 14:52:32 +0200

    Variante 1.1.1: Koordinaten der Kreise auf Bildschirm

--- src/Metrik/Main.java
+++ src/Metrik/Main.java
@@ -1,12 +1,8 @@
 package Metrik;
 
-import java.awt.Graphics;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Window window = new Window();
-		Graphics g = window.getGraphics();
-		System.out.println("Init abgeschlossen");
+		new Window();
 	}
 }
--- src/Metrik/Window.java
+++ src/Metrik/Window.java
@@ -11,22 +11,23 @@ import java.awt.event.MouseListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Window extends Frame implements MouseListener {
+public class Window extends Frame implements MouseListener,WindowListener {
 	int circlecount = 0;
 	Color color = Color.white;
 
 	public Window() {
-		this.addMouseListener(this);
-		
+		this.addMouseListener(this);	
+		addWindowListener(this);
 
 		// Fenster erstellen
 		setSize(500, 500);
-		setLayout(null);
 		setVisible(true);
 		setTitle("Metrik Version 1.0");
 
-		// Menüleiste anlegen
+		// Menüleiste anlegen, jedem Button einen ActionListener zuweisen
 		MenuBar mb = new MenuBar();
 		Menu menu = new Menu("Hintergrundfarbe");
 		MenuItem blue = new MenuItem("Blau");
@@ -49,6 +50,7 @@ public class Window extends Frame implements MouseListener {
 
 		mb.add(menu);
 		setMenuBar(mb);
+		setLocation(200,200);
 	}
 
 	ActionListener aliBlue = new ActionListener() {
@@ -92,33 +94,41 @@ public class Window extends Frame implements MouseListener {
 		}
 	};
 
-	@Override
 	public void mouseClicked(MouseEvent e) {
 		if (e.getClickCount() == 2) {
-			Graphics g = this.getGraphics();
-			paint(g, e.getX(), e.getY());
+			paint(this.getGraphics(), e.getX(), e.getY());
 		}
 	}
 
 	public void paint(Graphics g, int x, int y) {
 		Graphics2D g2 = (Graphics2D) g;
 		if (circlecount == 2) {
-			System.out.println("Feld geleert");
-//			g2.setBackground(Color.white);
-//			g2.setBackground(color);
-			
-			g2.setColor(color);
-			g2.fillRect(0, 0, 1920, 1080);
-			g2.setColor(Color.black);
+			clear(g2);
 			circlecount = 0;
 		} else {
-			System.out.println("Kreis " + ++circlecount);
 			// X und Y werden um die Hälfte der Größe des Kreises subtrahiert, damit der
 			// Mittelpunkt des Kreises genau auf dem Mauszeiger liegt
+			circlecount++;
 			g2.drawOval(x - 50, y - 50, 100, 100);
+			g2.drawString("X: " + x + ", Y: " + y, 20, 50 + (circlecount * 20));
 		}
 	}
+	
+	public void clear(Graphics2D g2) {
+		//Setze Farbe zum Zeichnen auf die aktuelle Hintergrundfarbe und zeichne ein ausgefülltes Rechteck auf größe des Bildschirms
+//		g2.setColor(color);
+//		g2.fillRect(0, 0, 1920, 1080);
+//		g2.setColor(Color.black);
+		setBackground(Color.white);
+		setBackground(color);
+		
+	}
 
+	public void windowClosing(WindowEvent windowEvent) {
+        //Schließe Fenster, wenn das rote X gedrückt wird
+        System.exit(0);
+    }
+	
 	@Override
 	public void mousePressed(MouseEvent e) {
 //		System.out.println("Mouse " + e.getClickCount() + " times pressed at " + e.getPoint());
@@ -137,6 +147,42 @@ public class Window extends Frame implements MouseListener {
 	@Override
 	public void mouseExited(MouseEvent e) {
 //		System.out.println("Mouse exited at " + e.getPoint());
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		dispose();
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
